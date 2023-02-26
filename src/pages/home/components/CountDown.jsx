31a55702ea31004249a9bee8e9f0b72b9233065f
Adil ABBADI <adil.abbadi@ifazs.gov.ma>
2023-02-26 02:24:53 +0100

    rfix countdown

--- src/pages/home/components/CountDown.jsx
+++ src/pages/home/components/CountDown.jsx
@@ -51,7 +51,7 @@ export const CountDown = (props)=>{
       <h2 className="u-text u-text-default u-text-1" data-animation-name="fadeIn" data-animation-duration="1000" data-animation-direction="Up"> Duathlon Tafraout arrive bientôt !</h2>
       <p className="u-text u-text-default u-text-2" data-animation-name="fadeIn" data-animation-duration="1000" data-animation-direction="Up">Le 6-7 mai 2023 à Tafraout.L'évènement "Duathlon Tafraout ⴷⵢⴰⵜⵍⵡⵏ ⵜⴰⴼⵔⴰⵡⵜ" <br/>offre l’opportunité de courir et faire du vélo entre les montagnes de l'Anti-Atlas du sud</p>
       <div className="u-countdown u-countdown-1" data-target-date="Sat, 01 Jan 2022 14:43:00 GMT" data-type="to-date" data-for="per-visitor" data-time-left="266h" data-timer-id="5ce0" data-target-number="100" data-direction="down" data-start-time="Wed, 22 Dec 2021 14:21:42 GMT" data-frequency="1s" data-after-count="none" data-redirect-url="https://">
-        <div className="u-countdown-wrapper u-spacing-30 row">
+        <div className=" u-spacing-30 row">
         
         
        
@@ -63,20 +63,20 @@ export const CountDown = (props)=>{
 
         
         
-          <div className="  u-countdown-item u-radius-22 u-spacing-10 u-white u-countdown-item-3 mt-2  col-md col-5">
+          <div className=" ms-3 u-countdown-item u-radius-22 u-spacing-10 u-white u-countdown-item-3 mt-2  col-md col-5">
             <div className="u-countdown-counter u-countdown-counter-3"><div className="u-countdown-number u-text-grey-75 px-5">{timeLeft.hours}</div></div>
             <div className="u-countdown-label u-countdown-label-3">Hours</div>
           </div>
          
           
-          <div className="u-countdown-item  u-radius-22 u-spacing-10 u-white u-countdown-item-4 mt-2  col-md col-5">
+          <div className="ms-3 u-countdown-item  u-radius-22 u-spacing-10 u-white u-countdown-item-4 mt-2  col-md col-5">
             <div className="u-countdown-counter u-countdown-counter-4"><div className="u-countdown-number u-text-grey-75 px-5">{timeLeft.minutes}</div></div>
             <div className="u-countdown-label u-countdown-label-4">Minutes</div>
           </div>
 
         
 
-          <div className="u-countdown-item  u-radius-22 u-spacing-10 u-white u-countdown-item-5 mt-2  col-md col-5">
+          <div className="ms-3 u-countdown-item  u-radius-22 u-spacing-10 u-white u-countdown-item-5 mt-2  col-md col-5">
             <div className="u-countdown-counter u-countdown-counter-5">
             
                 <div className="u-countdown-number u-text-grey-75 px-5">{timeLeft.seconds}</div></div>
